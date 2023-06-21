459e8ca5d52e5a6d71b698b3ac429c57e7c17ac5
Fabio <fabioalbertoaraujo@gmail.com>
2023-06-21 16:09:03 -0300

    Tarefa 2 - Estruturas condicionais e operadores - segunda correção

--- script.js
+++ script.js
@@ -24,21 +24,24 @@ let passed = false;
 // **sua lógica a partir daqui**
 
 
-// Testa a variável para comparar se tem o valor 'male'
+// Testa a variável gender para comparar se tem o valor 'male'
 if (gender == 'male') {
 
 
     //Testando se o valor da variável é maior ou igual a 1,70 
     if (height >= 1.70) {
 
-        //Testando se  a variavel tem o valor maior ou igual a 6 ou se a segunda variável é maior ou igual a 15 
-        if (barReps >= 6 || barSeconds >= 15) {
-            //Testando se a variavel tem o valor maior ou igual a 41
+        //Testando se a variavel barReps tem o valor maior ou igual a 6 ou se a segunda variável barSeconds é maior ou igual a 15 
+        if ((barReps >= 6) || (barSeconds >= 15)) {
+            
+            //Testando se a variavel abs tem o valor maior ou igual a 41
             if (abs >= 41) {
-                //Testando a igualdade entre as duas variáveis  e usa a condicional or para uma segunda alternativa de comparação de igualdade para ver se ambas as variáaveis cumprem o valor mínimo 
-                if (runDistance >= 3000 && runTime <= 720 || runDistance >= 5000 && runTime <= 1200) {
-                    //Testando a igualdade entre as duas variáveis e usa a condicional or para uma segunda alternativa para ver se a variável não ultrapassa o valor máximo 
-                    if (swimDistance >= 100 && swimTime <= 60 || diveTime <= 30) {
+            
+                //Testando a igualdade entre as duas variáveis runDistance tem o valor >= a 3000 e se a variável runTime tem o valor <= a 720 usa a condicional or para uma segunda alternativa de comparação de igualdade para ver se ambas as variáveis runDistance tem o valor >= 5000 e se runTime tem o valor <= 1200
+                if ((runDistance >= 3000 && runTime <= 720) || (runDistance >= 5000 && runTime <= 1200)) {
+            
+                    //Testando a igualdade entre as duas variáveis swimDistance para ver se tem o valor >= 100 e swimTime para ver se tem o valor <= 60, usa a condicional or como uma segunda alternativa para ver se a variável diveTime tem o valor <= 30 
+                    if ((swimDistance >= 100 && swimTime <= 60) || (diveTime <= 30)) {
                         
                         //Caso todas as condições sejam 'true', a variável passed recebe esse valor 
                         passed = true;
@@ -50,23 +53,23 @@ if (gender == 'male') {
 
 }
 
-// Testa a variável para comparar se tem o valor 'female'
+// Testa a variável gender para comparar se tem o valor 'female'
 if (gender == 'female') {
 
-    //Testando se o valor da variável é maior ou igual a 1,60 
+    //Testando se o valor da variável height é maior ou igual a 1,60 
     if (height >= 1.60) {
 
-        //Testando se  a variavel tem o valor maior ou igual a 5 ou se a segunda variável é maior ou igual a 12
-        if (barReps >= 5 || barSeconds >= 12) {
+        //Testando se a variavel barReps tem o valor maior ou igual a 6 ou se a segunda variável barSeconds é maior ou igual a 15 
+        if ((barReps >= 5) || (barSeconds >= 12)) {
 
-            //Testando se a variavel tem o valor maior ou igual a 41
+            //Testando se a variavel abs tem o valor maior ou igual a 41
             if (abs >= 41) {
 
-                //Testando a igualdade entre as duas variáveis  e usa a condicional or para uma segunda alternativa de comparação de igualdade para ver se ambas as variáaveis cumprem o valor mínimo 
-                if (runDistance >= 4000 && runTime <= 900 || runDistance >= 6000 && runTime <= 1320) {
+                //Testando a igualdade entre as duas variáveis runDistance tem o valor >= a 4000 e se a variável runTime tem o valor <= a 900 usa a condicional or para uma segunda alternativa de comparação de igualdade para ver se ambas as variáveis runDistance tem o valor >= 6000 e se runTime tem o valor <= 1320
+                if ((runDistance >= 4000 && runTime <= 900) || (runDistance >= 6000 && runTime <= 1320)) {
 
-                    //Testando a igualdade entre as duas variáveis e usa a condicional or para uma segunda alternativa para ver se a variável não ultrapassa o valor máximo 
-                    if (swimDistance >= 100 && swimTime <= 1 || diveTime <= 30) {
+                    //Testando a igualdade entre as duas variáveis swimDistance para ver se tem o valor >= 100 e swimTime para ver se tem o valor <= 60, usa a condicional or como uma segunda alternativa para ver se a variável diveTime tem o valor <= 30
+                    if ((swimDistance >= 100 && swimTime <= 60) || (diveTime <= 30)) {
                         //Caso todas as condições sejam 'true', a variável passed recebe esse valor 
                         passed = true
                     }
